ffd879fba5a13c269dbf9798d76909a55f1e2f92
Edy Segura <edysegura@gmail.com>
2021-12-23 00:09:24 -0300

    refactor: implement external employees seed

--- src/db/LocalDatabase.js
+++ src/db/LocalDatabase.js
@@ -1,5 +1,6 @@
 import Nullstack from 'nullstack';
 import Dexie from 'dexie';
+import { employeesSeed } from './employees-seed';
 
 class LocalDatabase extends Nullstack {
   static async start(context) {
@@ -11,32 +12,7 @@ class LocalDatabase extends Nullstack {
     });
 
     db.on('populate', async () => {
-      await db.employees.bulkPut([
-        {
-          name: 'Thomas Hardy',
-          email: 'thomashardy@gmail.com',
-          address: '89 Chiaroscuro Rd',
-          phone: '1715552222',
-        },
-        {
-          name: 'Dominique Perrier',
-          email: 'dominiqueperrier@mail.com',
-          address: 'Obere Str. 57, Berlin, Germany',
-          phone: '1715552222',
-        },
-        {
-          name: 'Maria Anders',
-          email: 'mariaanders@mail.com',
-          address: '25, rue Lauriston, Paris, France',
-          phone: '1715552222',
-        },
-        {
-          name: 'Fran Wilson',
-          email: 'franwilson@mail.com',
-          address: 'C/ Araquil, 67, Madrid, Spain',
-          phone: '1715552222',
-        },
-      ]);
+      await db.employees.bulkPut(employeesSeed);
     });
 
     db.open();
---
+++ src/db/employees-seed.js
@@ -0,0 +1,26 @@
+export const employeesSeed = [
+  {
+    name: 'Thomas Hardy',
+    email: 'thomashardy@gmail.com',
+    address: '89 Chiaroscuro Rd',
+    phone: '1715552222',
+  },
+  {
+    name: 'Dominique Perrier',
+    email: 'dominiqueperrier@mail.com',
+    address: 'Obere Str. 57, Berlin, Germany',
+    phone: '1715552222',
+  },
+  {
+    name: 'Maria Anders',
+    email: 'mariaanders@mail.com',
+    address: '25, rue Lauriston, Paris, France',
+    phone: '1715552222',
+  },
+  {
+    name: 'Fran Wilson',
+    email: 'franwilson@mail.com',
+    address: 'C/ Araquil, 67, Madrid, Spain',
+    phone: '1715552222',
+  },
+];
