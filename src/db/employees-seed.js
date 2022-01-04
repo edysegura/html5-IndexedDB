91e8187f75a23e917ffbb3f84db7360bf7066f0d
Edy Segura <edysegura@gmail.com>
2022-01-03 22:04:03 -0300

    chore: prettierfy

--- src/db/employees-seed.js
+++ src/db/employees-seed.js
@@ -1,19 +1,19 @@
-import faker from 'faker'
+import faker from 'faker';
 
 export const employeesSeed = generateEmployees();
 
 // TODO move it to WebWorker
 export function generateEmployees() {
-  const employees = []
+  const employees = [];
   for (let i = 0; i < 1000; i++) {
-    let firstName = faker.name.firstName() 
-    let lastName = faker.name.lastName()
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
     employees.push({
       name: `${firstName} ${lastName}`,
       email: faker.internet.email(firstName, lastName).toLowerCase(),
       address: faker.address.streetAddress(),
       phone: faker.phone.phoneNumberFormat(),
-    })
+    });
   }
-  return employees
+  return employees;
 }
