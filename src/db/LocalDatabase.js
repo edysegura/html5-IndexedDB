070a14442ccb0e82fbf4fe68eb2057c834ca2f70
Edy Segura <edysegura@gmail.com>
2022-01-05 07:37:49 -0300

    refactor: LocalDatabase

--- src/db/LocalDatabase.js
+++ src/db/LocalDatabase.js
@@ -4,8 +4,7 @@ import { generateEmployees } from './employees-seed';
 
 class LocalDatabase extends Nullstack {
   static async start(context) {
-    context._db = new Dexie('EmployeesDB');
-    const db = context._db;
+    const db = context._db = new Dexie('EmployeesDB');
 
     db.version(1).stores({
       employees: '++id,name,email,address,phone',
