47df3fedf7e74118f2d36f96a877a184165ce789
edysegura <edysegura@users.noreply.github.com>
2022-01-21 01:45:27 +0000

    Deploying to gh-pages from @ edysegura/html5-IndexedDB@7bbe52b4aca0e8e5cd9bec6300cf13b9d1b26ad9 🚀

---
+++ worker.js
@@ -0,0 +1,36 @@
+importScripts(
+  'https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js',
+);
+importScripts(
+  'https://cdnjs.cloudflare.com/ajax/libs/dexie/3.2.0/dexie.min.js',
+);
+
+function generateEmployees(numberOfRecords = 1000) {
+  const employees = [];
+  for (let i = 0; i < numberOfRecords; i++) {
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    employees.push({
+      name: `${firstName} ${lastName}`,
+      email: faker.internet.email(firstName, lastName).toLowerCase(),
+      address: faker.address.streetAddress(),
+      phone: faker.phone.phoneNumberFormat(),
+    });
+  }
+  return employees;
+}
+
+async function indexedDBSeed(numberOfRecords) {
+  const db = new Dexie('EmployeesDB');
+  db.version(1).stores({
+    employees: '++id,name,email,address,phone',
+  });
+  await db.employees.bulkPut(generateEmployees(numberOfRecords));
+}
+
+self.onmessage = async (event) => {
+  console.log('[Web Workers] Seeding employees database');
+  await indexedDBSeed(event.data);
+  self.postMessage('[Web Workers] Seeding has been completed!');
+  self.close();
+};
