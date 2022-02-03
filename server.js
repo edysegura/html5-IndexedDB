36d47928103c2a308282eee58247ba35639d009d
Edy Segura <edysegura@gmail.com>
2022-02-02 22:16:39 -0300

    Revert "try: update cdn and project root"
    This reverts commit 8d0117ec8ca6382aaa2634fd99b7f16cea2d00f0.

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ import Application from './src/Application';
 const context = Nullstack.start(Application);
 
 context.start = async function start() {
-  const { project } = context;
-  project.root = '/html5-IndexedDB'
-  project.cdn = 'https://edysegura.com/HTML5-IndexedDB/'
+  // https://nullstack.app/application-startup
 }
 
 export default context;
