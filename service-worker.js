2b901205849ccf35d94b73bb87017c193bfb3ca0
edysegura <edysegura@users.noreply.github.com>
2022-09-30 13:49:20 +0000

    Deploying to gh-pages from @ edysegura/html5-IndexedDB@2122182f36b4e600c79c10490bf71e2ade0db5e8 🚀

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.context = {
     "development": false,
     "production": true,
     "mode": "ssg",
-    "key": "7b1a07951539d15f287fa70c68e2af12de00de10",
+    "key": "682b587a71a699ecd3a5e9e9fdc131fa63edb44a",
     "name": ""
   },
   "project": {
